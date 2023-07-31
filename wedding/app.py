7f3a329ca1122bebbae215a983a77584bf9467de
Seah <code.angz@gmail.com>
2023-07-31 11:38:17 +0900

    0731

--- wedding/app.py
+++ wedding/app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+import math
+import datetime
+from flask_pymongo import PyMongo
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/local"
+mongo = PyMongo(app)
+
+@app.route('/write', methods=["POST"])
+def write():
+    name = request.form.get('name')
+    content = request.form.get('content')
+
+    mongo.db['wedding'].insert_one({
+        "name": name,
+        "content": content
+    })
+
+    return redirect('/')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    now = datetime.datetime.now()
+    wedding = datetime.datetime(2023, 7, 30, 0, 0, 0)
+    diff = (wedding - now).days
+
+    page = int(request.args.get('page', 1))
+    limit = 3
+    skip = (page - 1) * limit
+
+    count = mongo.db['wedding'].count_documents({})
+    max_page = math.ceil(count / limit)
+
+    pages = range(1, max_page + 1)
+
+    guestbooks = mongo.db['wedding'].find().limit(limit).skip(skip)
+
+    return render_template(
+        'index.html',
+        diff=diff,
+        guestbooks=guestbooks,
+        pages=pages
+    )
 
 if __name__ == '__main__':
     app.run()
