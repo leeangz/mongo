ca9ef149b60f9c5715a3107f49d43b66af58e411
Seah <code.angz@gmail.com>
2023-08-03 11:11:43 +0900

    0803 final done

--- namecard/app.py
+++ namecard/app.py
@@ -1,15 +1,10 @@
-import os
-from flask import Flask, request, render_template, redirect
-from flask.json import jsonify
-from flask_pymongo import PyMongo
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/local"
-mongo = PyMongo(app)
 
-@app.route('/make')
+@app.route('/')
 def main():
     return render_template('index.html')
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- namecard/static/main.js
+++ namecard/static/main.js
@@ -1,11 +1,18 @@
-const formName = document.querySelector(".form .name");
-const formTitle = document.querySelector(".form .title");
+function createCard() {
+  const name = document.querySelector(".name").value;
+  const title = document.querySelector(".title").value;
+  const phone = document.querySelector(".phone").value;
+  const email = document.querySelector(".email").value;
 
-const card = document.querySelector(".card");
-const cardName = document.querySelector(".card .name");
-const cardTitle = document.querySelector(".card .title .value");
+  const nameElement = document.querySelector(".card .name");
+  const titleElement = document.querySelector(".card .title .value");
+  const phoneElement = document.querySelector(".card .phone .value");
+  const emailElement = document.querySelector(".card .email .value");
 
-createCardBtn.addEventListener("click", function () {
-  card.classList.add("show");
-  cardName.innerHTML = formName.value;
-});
+  nameElement.innerText = name;
+  titleElement.innerText = title;
+  phoneElement.innerText = phone;
+  emailElement.innerText = email;
+
+  document.querySelector(".card").style.display = "flex";
+}
